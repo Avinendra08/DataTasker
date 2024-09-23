ca38f62e40b76612c130fefdc8582051c150db83
Avinendra <avinendra08@gmail.com>
2024-09-24 00:40:51 +0530

    backend setup done

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+//routes import
+//import farmerRouter from './routes/farmer.route.js'
+
+//routes declaration
+//app.use("/api/farmer/",farmerRouter)
+
+export { app }
