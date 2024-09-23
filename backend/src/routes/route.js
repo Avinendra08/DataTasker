9529320952650550659f7b4ad1c458cbc5a1f733
Avinendra <avinendra08@gmail.com>
2024-09-24 03:11:22 +0530

    message post and get apis done

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { getAllContacts, getContactById } from "../controllers/controller.js";
+import { getAllContacts, getContactById, getMessages, saveMessage } from "../controllers/controller.js";
 
 const router = Router();
 
 router.get("/contactlist",getAllContacts);
 router.get("/contacts/:id",getContactById);
+router.post("/messages/:id",saveMessage);
+router.get("/messagelist",getMessages);
 
 export default router;
